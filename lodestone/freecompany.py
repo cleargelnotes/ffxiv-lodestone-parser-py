5d1d69c07bb29c1809230ed2a3e679079723cc9b
Ralph Pineda <ralph.pineda@gmail.com>
2019-09-24 11:25:21 +0200

    Added fc member parsing

--- lodestone/freecompany.py
+++ lodestone/freecompany.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 url_info = "https://na.finalfantasyxiv.com/lodestone/freecompany/{}"
 url_members = "https://na.finalfantasyxiv.com/lodestone/freecompany/{}/member/"
 
+fc_id = "9233505136016441986"
+
 class FreeCompany(object):
     def __init__(self, fc_id, retrieve_data=False):
         self.fc_id = fc_id
@@ -30,9 +32,33 @@ class FreeCompany(object):
         }
         
     def retrieve_member_soups(self):
-        # TODO
-        return []
+        soup_0 = BeautifulSoup(requests.get(url_members.format(self.fc_id)).text, "html.parser")
+        ret = [soup_0]
+        # get total number of pages
+        li_pager = soup_0.find_all("li", {"class": "btn__pager__current"})[0]
+        pagestr = li_pager.next
+        totalpages = int(pagestr.split(" of ")[1])
+        for i in range(1, totalpages):
+            ret.append(BeautifulSoup(requests.get("{}?page={}".format(url_members.format(self.fc_id), i+1)).text, "html.parser"))
+        
+        return ret
         
     def parse_members(self, soups):
-        # TODO
-        pass
+        self.members = []
+        for soup in soups:
+            soup_uls = soup.find("div", {"class": "ldst__contents"}).find("div", {"class": "ldst__window"}).findChildren("ul")
+            for ul in soup_uls:
+                lis = ul.find_all("li", {"class": "entry"})
+                for entry in lis:
+                    char_id = entry.find("a", {"class": "entry__bg"}).attrs.get("href")[:-1].split("/")[-1]
+                    divdata = entry.find("div", {"class": "entry__freecompany__center"})
+                    if divdata:
+                        name = divdata.find("p", {"class": "entry__name"}).next
+                        rank_lis = divdata.find("ul", {"class": "entry__freecompany__info"}).find_all("li")
+                        rank = rank_lis[0].find("span").next
+                        if name and rank:
+                            self.members.append({
+                                "name": name,
+                                "rank": rank,
+                                "char_id": char_id
+                            })

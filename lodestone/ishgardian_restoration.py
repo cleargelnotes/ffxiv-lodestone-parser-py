d050b019911079e648e869b9bc6a95013bc641dc
Ralph Pineda <ralph.pineda@gmail.com>
2020-09-10 12:25:13 +0200

    Added float parsing

--- lodestone/ishgardian_restoration.py
+++ lodestone/ishgardian_restoration.py
@@ -16,10 +16,11 @@ def parse():
         for li in lis:
             world_name = str(li.find("span", {"class": "world_name"}).next)
             progress = li.find("div", {"class": "bar"}).find("span").attrs.get("style").split(" ")[1]
+            progress_float = float(progress[:-1])
             level = str(li.find("div", {"class": "status"}).find("p").next)
-            print(world_name + ": [" + level + "] " +progress)
+            #print(world_name + ": [" + level + "] " +progress)
             ret[world_name] = {
                 "level": level,
-                "progress": progress
+                "progress": progress_float
             }
     return ret
